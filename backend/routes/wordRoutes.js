8868741636a931b20e3aede348ceae13b63e5183
Lauri Luoma <lauluoma@gmail.com>
2024-12-04 19:27:33 +0200

    Add router.put to update words

--- backend/routes/wordRoutes.js
+++ backend/routes/wordRoutes.js
@@ -12,7 +12,7 @@ router.get("/words", async (req, res) => {
   }
 });
 
-// Route to delete a word
+// Route to delete a word by id
 router.delete("/words/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -46,6 +46,42 @@ router.post("/words", async (req, res) => {
       });
     }
     const result = await wordController.addWord(
+      english,
+      finnish,
+      swedish,
+      tags,
+    );
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to update a word by id
+router.put("/words/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { english, finnish, swedish, tags } = req.body;
+    if (!english) {
+      console.error("English translation is required");
+      return res.status(400).json({
+        error: "English translation is required",
+      });
+    }
+    if (!finnish) {
+      console.error("Finnish translation is required");
+      return res.status(400).json({
+        error: "Finnish translation is required",
+      });
+    }
+    if (!swedish) {
+      console.error("Swedish translation is required");
+      return res.status(400).json({
+        error: "Swedish translation is required",
+      });
+    }
+    const result = await wordController.updateWord(
+      id,
       english,
       finnish,
       swedish,
