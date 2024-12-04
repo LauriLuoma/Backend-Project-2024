24087b2407dfc954684806d73dabe9dbeaa0fecb
Lauri Luoma <lauluoma@gmail.com>
2024-12-04 19:15:25 +0200

    Add router.post for adding words

--- backend/routes/wordRoutes.js
+++ backend/routes/wordRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wordController = require("../controllers/wordController");
 
+// Route for all words
 router.get("/words", async (req, res) => {
   try {
     const words = await wordController.getAllWords();
@@ -11,10 +12,45 @@ router.get("/words", async (req, res) => {
   }
 });
 
+// Route to delete a word
 router.delete("/words/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const result = await wordController.deleteWord(id);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to add a new word
+router.post("/words", async (req, res) => {
+  try {
+    const { english, finnish, swedish, tags } = req.body;
+    if (!english) {
+      console.error("English translation is required");
+      return res.status(400).json({
+        error: "English translation is required",
+      });
+    }
+    if (!finnish) {
+      console.error("Finnish translation is required");
+      return res.status(400).json({
+        error: "Finnish translation is required",
+      });
+    }
+    if (!swedish) {
+      console.error("Swedish translation is required");
+      return res.status(400).json({
+        error: "Swedish translation is required",
+      });
+    }
+    const result = await wordController.addWord(
+      english,
+      finnish,
+      swedish,
+      tags,
+    );
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
