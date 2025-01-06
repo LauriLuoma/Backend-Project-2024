041bf9ca74a0bd6ada8d8eb7cea564c21d326f3f
Lauri Luoma <lauluoma@gmail.com>
2025-01-06 11:37:21 +0200

    Add handlers for addWord and deleteId

--- frontend/src/components/pages/Admin/Admin.jsx
+++ frontend/src/components/pages/Admin/Admin.jsx
@@ -1,6 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { addWord, deleteWord } from '../../api';
+
 
 function Admin() {
+  const [newWord, setNewWord] = useState({english: '', finnish: '', swedish: '', tags: ''});
+  const [deleteId, setDeleteId] = useState('');
+
+  const handleAddWordChange = (e) => {
+    const { name, value } = e.target;
+    setNewWord({ ...newWord, [name]: value });
+  };
+
+  const handleDeleteIdChange = (e) => {
+    setDeleteId(e.target.value);
+  };
+
+  const handleAddWordSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addWord(newWord);
+      console.log('Word added successfully');
+      setNewWord({ english: '', finnish: '', swedish: '', tags: '' });
+    } catch (error) {
+      console.error('Error adding word:', error);
+    }
+  };
+
+  const handleDeleteWordSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await deleteWord(deleteId);
+      console.log('Word deleted successfully');
+      setDeleteId('');
+    } catch (error) {
+      console.error('Error deleting word:', error);
+    }
+  };
 
   return (
     <div>
