1d2d2a12acf953775239c48cb7523baecf9a0f36
Lauri Luoma <lauluoma@gmail.com>
2025-01-06 16:17:44 +0200

    Add fetchWords

--- frontend/src/components/pages/Learn/Learn.jsx
+++ frontend/src/components/pages/Learn/Learn.jsx
@@ -1,6 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { getAllWords } from '../../../api';
 
 function Learn() {
+  const [words, setWords] = useState([]);
+
+  const fetchWords = async () => {
+    try {
+      const words = await getAllWords();
+      setWords(words);
+    } catch (error) {
+      console.error('Error fetching words:', error);
+    }
+  };
 
   return (
     <div>
