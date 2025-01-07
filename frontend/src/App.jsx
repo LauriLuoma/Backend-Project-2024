7a2857d66442a056c110969a2c271b59dcb63b8b
Lauri Luoma <lauluoma@gmail.com>
2025-01-07 18:25:31 +0200

    Update navbar width to 100vw

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,9 @@ import Admin from './components/pages/Admin/Admin';
 function App() {
   return (
     <Router>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw' }}>
         <Navbar />
-        <div style={{ width: '100%' }}>
+        <div style={{ width: '100%', marginTop: '20px' }}>
           <Routes>
             <Route path="/" element={<Learn />} />
             <Route path="/admin" element={<Admin />} />
